164ee6d40041cf4a0bab9051c132fb4b4aa5c53c
Phạm Văn Điệp <diep.job@outlook.com>
2020-01-02 09:15:13 +0700

    add socket io

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,24 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/configurations/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+    var io = require('socket.io')(strapi.server);
+    var users = [];
+    // listen for user connection
+    io.on('connection', (socket) => {
+        socket.user_id = (Math.random() * 100000000000000);
+        users.push(socket); // save the socket to use it later
+
+        // send message on user connection
+        socket.emit('hello', JSON.stringify({message: 'Hiiiiii!'}));
+        // listen for user diconnect
+        socket.on('disconnect', () => {
+            users.forEach((user, i) => {
+                // delete saved user when they disconnect
+                if(user.user_id === socket.user_id) users.splice(i, 1);
+            });
+        });
+    });
+    
+    strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
+};
