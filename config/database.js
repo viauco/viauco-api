a91c7f32b9c4f66a1e3549f0e75906c8a17e4304
Meo CHuot <diep.pham@amela.vn>
2020-06-24 16:14:00 +0700

    up

--- config/database.js
+++ config/database.js
@@ -4,8 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
+        /*client: 'sqlite',
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        */
+        'client': env('DATABASE_CLIENT','postgres'),
+        'database': env('DATABASE_NAME','db_name'),
+        'host': env('DATABASE_HOST','127.0.0.1'),
+        'port': env.int('DATABASE_PORT',5432),
+        'username': env('DATABASE_USERNAME','postgres'),
+        'password': env('DATABASE_PASSWORD','password'),
+        'schema': env('DATABASE_SCHEMA', 'public')
       },
       options: {
         useNullAsDefault: true,
