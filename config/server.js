3000f09f7b8635fe14a7a6a6c9de06b196124aa8
Meo CHuot <diep.pham@amela.vn>
2020-06-25 11:09:56 +0700

    up

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
+  host: process.env.HOST || '127.0.0.1',
   port: process.env.PORT || 1337,
   cron:{
     enable: env.bool('CRON_ENABLE', false)
