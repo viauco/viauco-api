b3916bd3e88e4798110a229b78db4210147ca250
Meo CHuot <diep.pham@amela.vn>
2020-06-25 11:08:18 +0700

    up

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-  //host: env('HOST', '0.0.0.0'),
-  //port: env.int('PORT', process.env.PORT || 1337),
+  host: '0.0.0.0',
+  port: process.env.PORT || 1337,
   cron:{
     enable: env.bool('CRON_ENABLE', false)
   },
   admin:{
     url: env('ADMIN_URL', '/__cms'),
-    autoOpen: env('ADMIN_AUTO_OPEN', true)
+    autoOpen: env('ADMIN_AUTO_OPEN', false)
   }
 });
