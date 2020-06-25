f6108b689b200a26502b08266d957ede78f4ae8e
Meo CHuot <diep.pham@amela.vn>
2020-06-25 11:00:09 +0700

    update host port config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 1337),
+  //host: env('HOST', '0.0.0.0'),
+  //port: env.int('PORT', process.env.PORT || 1337),
   cron:{
     enable: env.bool('CRON_ENABLE', false)
   },
