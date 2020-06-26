118854bc6b8beb195c8a85ebb2085f316581b0c8
Meo CHuot <diep.pham@amela.vn>
2020-06-26 07:16:15 +0700

    up

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,55 +1,81 @@
 'use strict';
 
+const { default: createStrapi } = require("strapi");
+
 /**
- * Lifecycle callbacks for the `blogs` model.
+ * Lifecycle callbacks for the `posts` model.
  */
 
 module.exports = {
   // Before saving a value.
   // Fired before an `insert` or `update` query.
-  // beforeSave: async (model, attrs, options) => {},
+  beforeSave: async (model, attrs, options) => {
+    strapi.io.emit('post_before_save', {model, attrs, options})
+  },
 
   // After saving a value.
   // Fired after an `insert` or `update` query.
-  // afterSave: async (model, response, options) => {},
+  afterSave: async (model, response, options) => {
+    strapi.io.emit('post_after_save', {model, response, options})
+  },
 
   // Before fetching a value.
   // Fired before a `fetch` operation.
-  // beforeFetch: async (model, columns, options) => {},
+  beforeFetch: async (model, columns, options) => {
+    strapi.io.emit('post_before_fetch', {model, columns, options})
+  },
 
   // After fetching a value.
   // Fired after a `fetch` operation.
-  // afterFetch: async (model, response, options) => {},
+  afterFetch: async (model, response, options) => {
+    strapi.io.emit('post_after_fetch', {model, response, options})
+  },
 
   // Before fetching all values.
   // Fired before a `fetchAll` operation.
-  // beforeFetchAll: async (model, columns, options) => {},
+  beforeFetchAll: async (model, columns, options) => {
+    strapi.io.emit('post_before_fetch_all', {model, columns, options})
+  },
 
   // After fetching all values.
   // Fired after a `fetchAll` operation.
-  // afterFetchAll: async (model, response, options) => {},
+  afterFetchAll: async (model, response, options) => {
+    strapi.io.emit('post_after_fetch_all', {model, response, options})
+  },
 
   // Before creating a value.
   // Fired before an `insert` query.
-  // beforeCreate: async (model, attrs, options) => {},
+  beforeCreate: async (model, attrs, options) => {
+    strapi.io.emit('post_before_create', {model, attrs, options})
+  },
 
   // After creating a value.
   // Fired after an `insert` query.
-  // afterCreate: async (model, attrs, options) => {},
+  afterCreate: async (model, attrs, options) => {
+    strapi.io.emit('post_after_create', {model, attrs, options})
+  },
 
   // Before updating a value.
   // Fired before an `update` query.
-  // beforeUpdate: async (model, attrs, options) => {},
+  beforeUpdate: async (model, attrs, options) => {
+    strapi.io.emit('post_before_update', {model, attrs, options})
+  },
 
   // After updating a value.
   // Fired after an `update` query.
-  // afterUpdate: async (model, attrs, options) => {},
+  afterUpdate: async (model, attrs, options) => {
+    strapi.io.emit('post_after_update', {model, attrs, options})
+  },
 
   // Before destroying a value.
   // Fired before a `delete` query.
-  // beforeDestroy: async (model, attrs, options) => {},
+  beforeDestroy: async (model, attrs, options) => {
+    strapi.io.emit('post_before_destroy', {model, attrs, options})
+  },
 
   // After destroying a value.
   // Fired after a `delete` query.
-  // afterDestroy: async (model, attrs, options) => {}
+  afterDestroy: async (model, attrs, options) => {
+    strapi.io.emit('post_after_destroy', {model, attrs, options})
+  }
 };
