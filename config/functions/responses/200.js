4fab93157d5d73373c37feaf713133d1a3285cc0
CUly247 <diep.pham@amela.vn>
2020-06-26 20:24:35 +0700

    up

---
+++ config/functions/responses/200.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = async (ctx) => {
+    //console.log(ctx.response)
+    /*ctx.response.body = {
+        success: 1,
+        code: 200,
+        data: ctx.response.body
+    };*/
+};
